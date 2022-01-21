79eac1101472e0cc3981cc3d1d6360fa541b359c
Kresten Laust <kressckerl@gmail.com>
2022-01-21 15:31:46 +0100

    Tilføjede gitignore og fjernede træls filer

--- WifiFinderSystem/WifiFinderSystem.cs
+++ WifiFinderSystem/WifiFinderSystem.cs
@@ -6,64 +6,64 @@ using System.Threading.Tasks;
 
 namespace WifiFinderSystem
 {
-    public class WifiFinderSystem
-    {
-        public readonly struct Data
+	public class WifiFinderSystem
+	{
+		public readonly struct Data
 		{
-            public readonly byte ID;
-            public readonly DateTime Timestamp;
-            public readonly byte RSSi;
+			public readonly byte ID;
+			public readonly DateTime Timestamp;
+			public readonly byte RSSi;
 
-            public Data(byte ID, DateTime Timestamp, byte RSSi)
+			public Data(byte ID, DateTime Timestamp, byte RSSi)
 			{
-                this.ID = ID;
-                this.Timestamp = Timestamp;
-                this.RSSi = RSSi;
+				this.ID = ID;
+				this.Timestamp = Timestamp;
+				this.RSSi = RSSi;
 			}
 		}
 
-        static Dictionary<long, Queue<Data>> dataDictionary = new Dictionary<long, Queue<Data>>();
+		static Dictionary<long, Queue<Data>> dataDictionary = new Dictionary<long, Queue<Data>>();
 
-        public static void RefreshData(int maxAge)
+		public static void RefreshData(int maxAge)
 		{
-            DateTime nowOffset = DateTime.Now.Subtract(TimeSpan.FromSeconds(maxAge));
+			DateTime nowOffset = DateTime.Now.Subtract(TimeSpan.FromSeconds(maxAge));
 
 			foreach (KeyValuePair <long, Queue<Data>> singleDevice in dataDictionary)
 			{
-                long deviceAddress = singleDevice.Key;
-                Queue<Data> devicePackets = singleDevice.Value;
+				long deviceAddress = singleDevice.Key;
+				Queue<Data> devicePackets = singleDevice.Value;
 
-                while (devicePackets.Count != 0 && devicePackets.Peek().Timestamp < nowOffset)
+				while (devicePackets.Count != 0 && devicePackets.Peek().Timestamp < nowOffset)
 				{
-                    devicePackets.Dequeue();
+					devicePackets.Dequeue();
 				}
 			}
 		}
 
-        public static int CountDataEntries()
+		public static int CountDataEntries()
 		{
-            int num = 0;
-            foreach (KeyValuePair<long, Queue<Data>> singleDevice in dataDictionary)
-            {
-                long deviceAddress = singleDevice.Key;
-                Queue<Data> devicePackets = singleDevice.Value;
-
-                num += devicePackets.Count;
-			}
-            return num;
-		}
-
-        public static void AddData(long MacAddress, byte ID, byte RSSi)
-		{
-            Queue<Data> dataList;
-
-            if (!dataDictionary.TryGetValue(MacAddress, out dataList))
+			int num = 0;
+			foreach (KeyValuePair<long, Queue<Data>> singleDevice in dataDictionary)
 			{
-                dataList = new Queue<Data>();
-                dataDictionary[MacAddress] = dataList;
+				long deviceAddress = singleDevice.Key;
+				Queue<Data> devicePackets = singleDevice.Value;
+
+				num += devicePackets.Count;
+			}
+			return num;
+		}
+
+		public static void AddData(long MacAddress, byte ID, byte RSSi)
+		{
+			Queue<Data> dataList;
+
+			if (!dataDictionary.TryGetValue(MacAddress, out dataList))
+			{
+				dataList = new Queue<Data>();
+				dataDictionary[MacAddress] = dataList;
 			}
 
-            dataList.Enqueue(new Data(ID, DateTime.Now, RSSi));
+			dataList.Enqueue(new Data(ID, DateTime.Now, RSSi));
 		}
-    }
+	}
 }
